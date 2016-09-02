1b8aa1401c96ccb65e2172b62fa721c4702782e3
Chuvi <Chuvi.w@gmail.com>
2016-09-03 00:55:38 +0300

    Initial Commit

---
+++ TubeTest.v
@@ -0,0 +1,19 @@
+module TubeTest
+
+(
+	
+	input clk,
+	input reg Key[3:0],
+	input reg Sw[7:0],
+	
+	output reg Dot,
+	output reg Seg[6:0],
+	output reg Led[0:7],
+	
+);
+
+	
+
+	// Module Item(s)
+
+endmodule
