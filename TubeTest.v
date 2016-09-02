2b850292c34a75d8feb7327f30c5c786f3d167ee
Chuvi <Chuvi.w@gmail.com>
2016-09-03 02:42:47 +0300

    ...

--- TubeTest.v
+++ TubeTest.v
@@ -1,19 +1,31 @@
-module TubeTest
+module TubeTest(clk,Key,Sw,Dot,Seg,Led);
 
-(
-	
-	input clk,
-	input reg Key[3:0],
-	input reg Sw[7:0],
-	
-	output reg Dot,
-	output reg Seg[6:0],
-	output reg Led[0:7],
-	
-);
+input clk;
+input [3:0] Key;
+input [7:0] Sw;
+output Dot;
+output [6:0] Seg;
+output [7:0] Led ;
+wire [3:0] Key;
+wire [7:0] Sw;
+wire Dot;
+reg [6:0] Seg;
+reg [7:0] Led ;
+
+reg [7:0] CurLed;
 
 	
+assign Dot=1'b1;
 
-	// Module Item(s)
+always @ ( posedge clk )
+ begin
+  CurLed<=CurLed+1;  
+ end
+ 
+always @ ( posedge clk )
+ begin
+  Led<=(1<<CurLed);
+  Seg<=CurLed;
+ end
 
 endmodule
